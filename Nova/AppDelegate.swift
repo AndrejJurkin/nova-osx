38961f7f8048b7e20d5b02bdc4408dac60c3d132
Andrej Jurkin <andrej.jurkin@gmail.com>
2017-09-02 12:55:07 +0200

    Init project

---
+++ Nova/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nova
+//
+//  Created by Andrej Jurkin on 9/2/17.
+//  Copyright © 2017 Andrej Jurkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nova/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nova
+//
+//  Created by Andrej Jurkin on 9/2/17.
+//  Copyright © 2017 Andrej Jurkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
