e4df78370df2008d5eae4ebb35edd617bda214e5
Andrej Jurkin <andrej.jurkin@gmail.com>
2017-09-13 19:10:16 +0200

    Implement user preferences

---
+++ Nova/Prefs.swift
@@ -0,0 +1,74 @@
+//
+//  Prefs.swift
+//  Nova
+//
+//  Created by Andrej Jurkin on 13/09/17.
+//  Copyright © 2017 Andrej Jurkin. All rights reserved.
+//
+
+import Foundation
+
+/// Class for accessing and modifying preference data
+/// returned by NSUserDefaults.
+///
+/// All clients should use a shared instance of this class
+class Prefs {
+    
+    static let shared = Prefs()
+    
+    /// Key values for acessing and modifying user defaults data
+    struct Key {
+        
+        static let refreshInterval = "refresh_interval"
+        
+        static let pinnedSymbols = "pinned_symbols"
+        
+        static let targetCurrency = "target_currency"
+    }
+    
+    let userDefaults = UserDefaults.standard
+    
+    /// Ticker refresh interval (seconds), default 30.0
+    var rereshInterval: Float {
+        
+        get {
+            return self.userDefaults.float(forKey: Key.refreshInterval)
+        }
+        
+        set {
+            self.userDefaults.set(newValue, forKey: Key.refreshInterval)
+        }
+    }
+    
+    /// Ticker symbols pinned to be displayed in menu bar
+    var pinedSymbols: [String]? {
+        
+        get {
+            return self.userDefaults.array(forKey: Key.pinnedSymbols) as? [String]
+        }
+        
+        set {
+            self.userDefaults.set(newValue, forKey: Key.pinnedSymbols)
+        }
+    }
+    
+    /// Target currency to convert all coins into, default USD
+    var targetCurrency: String {
+        
+        get {
+            if let target = self.userDefaults.string(forKey: Key.targetCurrency) {
+                return target
+            }
+            
+            return "USD"
+        }
+        
+        set {
+            self.userDefaults.set(newValue, forKey: Key.targetCurrency)
+        }
+    }
+    
+    private init() {
+        
+    }
+}
